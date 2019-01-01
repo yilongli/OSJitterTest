1e6a508e148b757c982a4735d24fce949cc65bc8
Yilong Li <yilongl@cs.stanford.edu>
2018-12-31 18:38:20 -0800

    reduce the jitters due to CPU power management programmatically

--- user_loop.cc
+++ user_loop.cc
@@ -1,27 +1,47 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <stdint.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <x86intrin.h>
 
 #define unlikely(x) __builtin_expect((x),0)
 
 #define THRESHOLD_TICKS 30000
 #define THRESHOLD_COUNT 100
 
-static __inline __attribute__((always_inline))
-uint64_t rdtsc()
-{
-    uint32_t tsc_aux;
-    return __builtin_ia32_rdtscp(&tsc_aux);
-}
+static int pm_qos_fd = -1;
+
+struct PowerManagementQoS {
+  public:
+    PowerManagementQoS()
+    {
+        uint32_t target = 0;
+        if (pm_qos_fd >= 0) return;
+        pm_qos_fd = open("/dev/cpu_dma_latency", O_RDWR);
+        if (pm_qos_fd < 0) {
+            fprintf(stderr, "Failed to open PM QOS file: %s", strerror(errno));
+            exit(errno);
+        }
+        write(pm_qos_fd, &target, sizeof(target));
+    }
+
+    ~PowerManagementQoS()
+    {
+        if (pm_qos_fd >= 0) close(pm_qos_fd);
+    }
+};
 
 uint64_t jitter[THRESHOLD_COUNT] = {};
 uint64_t timestamp[THRESHOLD_COUNT] = {};
 
 int main(int argc, char* argv[])
 {
+    PowerManagementQoS pmQoS;
     pid_t ppid;
     if (argc == 1) {
         // Not running in snapshot mode.
@@ -40,9 +60,9 @@ int main(int argc, char* argv[])
     }
 
     uint32_t cnt = 0;
-    uint64_t t0 = rdtsc();
+    uint64_t t0 = __rdtsc();
     while (1) {
-        uint64_t t1 = rdtsc();
+        uint64_t t1 = __rdtsc();
         if (unlikely(t1 - t0 > THRESHOLD_TICKS)) {
             jitter[cnt] = t1 - t0;
             timestamp[cnt] = t1;
@@ -51,9 +71,11 @@ int main(int argc, char* argv[])
                 if (ppid > 0) {
                     kill(ppid, SIGUSR2);
                 }
+                uint64_t lastTsc = 0;
                 for (uint32_t i = 0; i < THRESHOLD_COUNT; i++) {
-                    printf("jitter %u: %lu cycles, timestamp %lu\n",
-                            i, jitter[i], timestamp[i]);
+                    printf("jitter %u: %lu cycles, timestamp %lu, delta %lu\n",
+                            i, jitter[i], timestamp[i], timestamp[i] - lastTsc);
+                    lastTsc = timestamp[i];
                 }
                 return 0;
             }
